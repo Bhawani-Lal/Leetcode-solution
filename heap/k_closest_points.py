9bfd6adab7cd33f7179cf2b998891f2f4164e752
Bhawani Lal <bhawanilalbxr@gmail.com>
2026-07-08 22:37:58 +0100

    Implement kClosest method to find closest points

---
+++ heap/k_closest_points.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        heap = []
+
+        for x, y in points:
+            distance = x*x + y*y
+
+            heapq.heappush(heap,(distance,[x, y]))
+        
+        result = []
+        for i in range(k):
+            distance, point = heapq.heappop(heap)
+            result.append(point)
+        
+        return result
